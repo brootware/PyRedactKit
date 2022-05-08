05f0623be128dd637808e5b79e82cddf334e431a
rootware <ominbruce@outlook.com>
2022-05-08 08:43:35 +0800

    added todo

--- pyredactkit/redact.py
+++ pyredactkit/redact.py
@@ -88,6 +88,7 @@ class Redactor:
         Returns:
             redacted_line (str): redacted line
         """
+        # Refactor this to loop through and check for option.
         redacted_line = ''
         if option in ("dns", "domain"):
             dns = id_object.regexes[1]['pattern']
