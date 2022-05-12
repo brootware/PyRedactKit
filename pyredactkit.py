d182fa393957e0ddfecfac308e14b57e5bdcece8
rootware <ominbruce@outlook.com>
2022-05-12 08:18:02 +0800

    added process_report function

--- pyredactkit.py
+++ pyredactkit.py
@@ -71,6 +71,7 @@ def main():
             redact_obj.process_file(file, args.redactiontype, args.dirout)
         else:
             redact_obj.process_file(file)
+            redact_obj.process_report(file)
 
 
 if __name__ == "__main__":
