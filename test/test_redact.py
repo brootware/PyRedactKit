c21b4115b0d80880c6c3ba881e38dba0dfe2c2c7
rootware <ominbruce@outlook.com>
2022-05-23 17:04:39 +0800

    added more test for unredaction

--- test/test_redact.py
+++ test/test_redact.py
@@ -1,5 +1,7 @@
 import pytest
 from pyredactkit.redact import Redactor
+from pyredactkit.unredact import Unredactor
+from pyredactkit.identifiers import Identifier
 
 data = """John, please get that article on www.linkedin.com to me by 5:00PM on Jan 9th 2012. 4:00 would be ideal, actually. If you have any questions, You can reach me at(519)-236-2723 or get in touch with my associate at harold.smith@gmail.com
 this is my IP: 102.23.5.1
@@ -42,30 +44,52 @@ Card_Number,Card_Family,Credit_Limit,Cust_ID
 people_names = "John,Jones,Alex,Bruce"
 mask_names = "\u2588" * 15
 count_names = 4
+key_mapping = {
+    "2b21e939-abc9-4d06-9c5a-de9bf2aa22b4": "10.10.10.1"}
+redacted_text = "My router is : 2b21e939-abc9-4d06-9c5a-de9bf2aa22b4"
+unredacted_text = "My router is : 10.10.10.1"
+
 
 @pytest.fixture
-def obj():
+def redactor_obj():
     return Redactor()
 
 
-def test_check_python_file(obj):
-    assert obj.check_file_type(
+@pytest.fixture
+def unredactor_obj():
+    return Unredactor()
+
+
+@pytest.fixture
+def identifier_obj():
+    return Identifier()
+
+
+def test_check_python_file(redactor_obj):
+    assert redactor_obj.check_file_type(
         __file__) == 'text/x-python', 'Failed python file check'
 
 
-def test_number_of_allowed_types(obj):
-    assert len(obj.get_allowed_files()
+def test_number_of_allowed_types(redactor_obj):
+    assert len(redactor_obj.get_allowed_files()
                ) == 10, 'Number of allowed tests does not match expected'
 
 
-def test_current_file_is_allowed(obj):
-    assert obj.allowed_file(__file__), f'{__file__} should be allowed'
+def test_current_file_is_allowed(redactor_obj):
+    assert redactor_obj.allowed_file(__file__), f'{__file__} should be allowed'
 
 
-def test_redact_function_should_return_string(obj):
-    assert type(obj.redact(data)) == str, 'redact function should return string'
+def test_valid_option_function_should_return_tuple(redactor_obj):
+    assert type(redactor_obj.valid_options()
+                ) == tuple, 'redact function should return tuple'
 
 
-def test_redact_name_function_should_return_string_and_integer(obj):
-    set1 = (f'{mask_names},{mask_names},{mask_names},{mask_names}',count_names)
-    assert obj.redact_name(people_names) == set1, 'redact_name function should return masked data and count'
+def test_redact_name_function_should_return_string_and_integer(redactor_obj):
+    set1 = (f'{mask_names},{mask_names},{mask_names},{mask_names}', count_names)
+    assert redactor_obj.redact_name(
+        people_names) == set1, 'redact_name function should return masked data and count'
+
+
+def test_replace_all_function_should_return_unredacted_string(unredactor_obj):
+    assert unredactor_obj.replace_all(
+        redacted_text, key_mapping) == unredacted_text, 'replace_all function should return unredacted string'
