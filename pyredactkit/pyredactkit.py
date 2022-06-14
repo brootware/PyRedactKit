caa11b0925580432fb4105d831343263385edc7a
rootware <ominbruce@outlook.com>
2022-06-14 11:49:13 +0800

    fixed failing test for argparser

--- pyredactkit/pyredactkit.py
+++ pyredactkit/pyredactkit.py
@@ -31,56 +31,6 @@ banner = """
     https://brootware.github.io                                                                             
     """
 
-parser = argparse.ArgumentParser(
-    description='Supply a sentence or paragraph to redact sensitive data from it. Or read in a file or set of files with -f , and return the result.',
-    formatter_class=argparse.ArgumentDefaultsHelpFormatter
-)
-
-parser.add_argument(
-    "text",
-    type=str,
-    help="""Redact sensitive data of a sentence from command prompt.""",
-    nargs="*"
-)
-parser.add_argument(
-    "-f",
-    "--file",
-    nargs="+",
-    help="""
-            Path of a file or a directory of files.
-            Usage: pyredactkit [file/filestoredact]"""
-)
-parser.add_argument(
-    "-u",
-    "--unredact",
-    help="""
-            Option to unredact masked data.
-            Usage: pyredactkit -f [redacted_file] -u [.hashshadow.json]
-            """
-)
-parser.add_argument(
-    "-d",
-    "--dirout",
-    help="""
-            Output directory of the file.
-            Usage: pyredactkit -f [file/filestoredact] -d [redacted_dir]
-            """
-)
-parser.add_argument(
-    '-r',
-    '--recursive',
-    action='store_true',
-    default=True,
-    help='Search through subfolders'
-)
-parser.add_argument(
-    '-e',
-    '--extension',
-    default='',
-    help='File extension to filter by.'
-)
-args = parser.parse_args()
-
 
 def execute_file_arg():
     full_paths = [os.path.join(os.getcwd(), path) for path in args.file]
@@ -107,6 +57,55 @@ def execute_file_arg():
 
 def main():
     print(banner)
+
+    parser = argparse.ArgumentParser(
+        description='Supply a sentence or paragraph to redact sensitive data from it. Or read in a file or set of files with -f , and return the result.',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+    )
+    parser.add_argument(
+        "text",
+        type=str,
+        help="""Redact sensitive data of a sentence from command prompt.""",
+        nargs="*"
+    )
+    parser.add_argument(
+        "-f",
+        "--file",
+        nargs="+",
+        help="""
+                Path of a file or a directory of files.
+                Usage: pyredactkit [file/filestoredact]"""
+    )
+    parser.add_argument(
+        "-u",
+        "--unredact",
+        help="""
+                Option to unredact masked data.
+                Usage: pyredactkit -f [redacted_file] -u [.hashshadow.json]
+                """
+    )
+    parser.add_argument(
+        "-d",
+        "--dirout",
+        help="""
+                Output directory of the file.
+                Usage: pyredactkit -f [file/filestoredact] -d [redacted_dir]
+                """
+    )
+    parser.add_argument(
+        '-r',
+        '--recursive',
+        action='store_true',
+        default=True,
+        help='Search through subfolders'
+    )
+    parser.add_argument(
+        '-e',
+        '--extension',
+        default='',
+        help='File extension to filter by.'
+    )
+    args = parser.parse_args()
 
     if args.file or (args.file and args.dirout):
         execute_file_arg()
