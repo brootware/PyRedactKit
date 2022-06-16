564f8f64d1bdcb3118981cb5d15fa529de66ba89
rootware <ominbruce@outlook.com>
2022-06-16 20:20:24 +0800

    added sonar python version

--- tests/test_redact.py
+++ tests/test_redact.py
@@ -1,5 +1,4 @@
 import pytest
-import os
 from pyredactkit.redact import Redactor
 
 data = """John, please get that article on www.linkedin.com to me by 5:00PM on Jan 9th 2012. 4:00 would be ideal, actually. If you have any questions, You can reach me at(519)-236-2723 or get in touch with my associate at harold.smith@gmail.com
